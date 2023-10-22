09319c35c455bd4eb93211af8d0ea08c22ef2218
aent <alintssr@gmail.com>
2023-10-21 22:09:08 -0400

    formatted description and added color variables

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,11 +1,27 @@
-
-
+import './Description.scss'
+import Views from '../../assets/icons/Icons/views.svg'
+import Likes from '../../assets/icons/Icons/likes.svg'
 
 export const Description = ({videoDetails}) => {
     return (
-        <div>
-            <h1>{videoDetails.title}</h1>
-            <p>{videoDetails.description}</p>
+        <div className="description">
+            <h1 className="description__title">{videoDetails.title}</h1>
+            <div className='description__container'>
+                <div className='description__subcontainer'>
+                    <div className="description__top">
+                        <span className='description__channel'>By {videoDetails.channel}</span>
+                        <span><img className='description__views--icon' src={Views} alt="views icon"/></span>
+                        <span className='description__views'>{videoDetails.views}</span>
+                    </div>
+                    <div className='description__bottom'>
+                        <span className='description__date'>{videoDetails.timestamp}</span>
+                        <span><img className='description__likes--icon' src={Likes} alt="views likes"/></span>
+                        <span className='description__likes'>{videoDetails.likes}</span>
+                    </div>
+                </div>
+               
+            </div>
+            <p className="description__description">{videoDetails.description}</p>
         </div>
     )
 }
