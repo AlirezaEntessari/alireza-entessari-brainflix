ebda1daa520c942fbe260c3a84bdebdf403440e4
aent <alintssr@gmail.com>
2023-10-22 04:50:59 -0400

    add scss styling

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,12 +1,17 @@
 import './Comment.scss'
+import Image from '../../assets/images/Images/Mohan-muruge.jpg'
 
 const Comment = ( { name, comment, timestamp } ) => {
     return (
 
         <div className="comment">
-            <div className="comment__name">{ name }</div>
+            <div className="comment__container">
+                <img className='comment__image' src="" alt="" />
+                <div className="comment__name">{ name }</div>
+                <div className="comment__timestamp">{ timestamp }</div>
+            </div>
+          
             <div className="comment__comment">{ comment }</div>
-            <div className="comment__timestamp">{ timestamp }</div>
         </div>
        
     )
