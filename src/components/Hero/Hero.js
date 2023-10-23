b1832f3e2214ffe91cf076e7393f9b40cdb68bd3
aent <alintssr@gmail.com>
2023-10-22 20:16:26 -0400

    added padding and dark black background color for hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,12 @@
-import videoDetails from '../../data/Data/video-details.json'
-import { useState } from 'react';
 import './Hero.scss'
 
 const Hero = ( { image } ) => {
     return (
         <>
-        <div className="hero">
-            <video className='hero__video' poster={ image }></video>
-        </div>
+            <div className="hero">
+                <video className='hero__video' poster={ image }></video>
+            </div>
        
-
         </>
     )
 }
