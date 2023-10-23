773d4aa43772d5a3dbac1dc0b0e2115e3e5326ab
aent <alintssr@gmail.com>
2023-10-22 21:05:21 -0400

    position sidebar on right and other formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import './App.css';
 import Header from './components/Header/Header';
 import { CommentList } from './components/Comment-List/CommentList';
 import videoData from "./data/Data/video-details.json"
@@ -8,6 +7,7 @@ import sidebarData from './data/Data/videos.json';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Description } from './components/Description/Description';
 import { AddComment } from './components/AddComment/AddComment';
+import './App.scss'
  
 
 function App() {
@@ -20,10 +20,16 @@ function App() {
     <div className="App">
       <Header/>
       <Hero image={videoDetails.image}/>
-      <Description videoDetails={videoDetails}/>
-      <AddComment/>
-      <CommentList comments={videoDetails.comments}/>
-      <Sidebar sideBarDetails={sideBarDetails}/>
+      <div className='App__container'>
+        <div className="App__subcontainer">
+          <Description videoDetails={videoDetails}/>
+          <AddComment/>
+          <CommentList comments={videoDetails.comments}/>
+        </div>
+        <div className="App__sidebar">
+          <Sidebar sideBarDetails={sideBarDetails}/>
+        </div>
+      </div>
       
     </div>
   );
